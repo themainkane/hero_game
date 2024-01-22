ed038bfb7358c7d3862a3e8f6209f4a0c7345b89
Joe <joetkane@gmail.com>
2024-01-22 13:43:21 +0000

    basic styling

---
+++ js/game.js
@@ -0,0 +1,11 @@
+const hero = document.querySelector('.hero');
+hero.style.top = '500px';
+hero.style.left = '100px';
+
+const ghost = document.querySelector('.ghost');
+ghost.style.top = '200px';
+ghost.style.left = '600px';
+
+const treasure = document.querySelector('.treasure');
+treasure.style.top = '0px';
+treasure.style.left = '400px';
